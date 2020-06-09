d905ae7e4fc294c71afe2b15008a4e00f7c61bac
MoistSenpai <supercrab115@gmail.com>
2020-06-09 07:24:05 -0400

    Added register page

--- Frontend/client/src/App.tsx
+++ Frontend/client/src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/navbar';
 import Login from './pages/login';
 import Home from './pages/home';
 import Error from './pages/error';
+import Register from './pages/register';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
 
         <Route path="/" exact component={Home} />
         <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
         <Route component={Error} />
 
       </Switch>
