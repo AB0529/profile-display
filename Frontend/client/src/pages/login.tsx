873371edf3440880c273ffd5ae78860fbfa99023
esarnb <esarnb@gmail.com>
2020-06-11 23:52:22 -0400

    Fixing padding of cards to fit to page on resize

--- Frontend/client/src/pages/login.tsx
+++ Frontend/client/src/pages/login.tsx
@@ -9,27 +9,23 @@ export default function Login() {
   return (
     <div className="LoginOuter">
       <DarkModeButton />
-
-
-
         <div className="LoginInner">
-        <Card className="LoginCard"  interactive={true} elevation={Elevation.TWO}>
-          <h2 className="LoginTitle"> Login Page </h2>
+          <Card className="LoginCard"  interactive={true} elevation={Elevation.TWO}>
+            <h2 className="LoginTitle"> Login Page </h2>
+                
+              <FormGroup 
+                helperText=""
+                label=""
+                labelFor="text-input"
+                labelInfo="(required)"
+                ><Icon icon="log-in" /> Username
+                <InputGroup id="text-input" placeholder="Einstein42" />
+                <Icon icon="key" /> Password
+                <InputGroup type="password" id="text-input" placeholder="Password" />
+              </FormGroup>   
               
-            <FormGroup 
-              helperText=""
-              label=""
-              labelFor="text-input"
-              labelInfo="(required)"
-              ><Icon icon="log-in" /> Username
-              <InputGroup id="text-input" placeholder="Einstein42" />
-              <Icon icon="key" /> Password
-              <InputGroup type="password" id="text-input" placeholder="Password" />
-            </FormGroup>   
-            
-            <Button>Submit</Button>
-        </Card>
-         
+              <Button>Submit</Button>
+          </Card>
         </div>
     </div>
   );
